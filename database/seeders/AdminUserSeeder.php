af0d7ab62deec393b3bef140c6a8d198c99a20dc
Sch39 <sukronraezpactoer@gmail.com>
2023-06-16 10:40:20 +0700

    init push

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class AdminUserSeeder extends Seeder
 {
@@ -14,6 +17,16 @@ class AdminUserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $adminRole = Role::create(['name' => 'admin']);
+        $permission = Permission::create(['name' => 'manage article']);
+        $permission->assignRole($adminRole);
+
+        $adminUser = User::create([
+            'name' => 'sukron@sch39.dev',
+            'email' => 'sukron@sch39.dev',
+            'password' => bcrypt('sch39'),
+        ]);
+
+        $adminUser->assignRole('admin');
     }
 }
